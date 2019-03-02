47f2d4fac993a947ec86e103b1e0793413da5b64
JenioPY <jeniopy@gmail.com>
2019-03-02 19:00:17 +0100

    Flake8 fixes.

--- compimg/exceptions.py
+++ compimg/exceptions.py
@@ -1,12 +1,16 @@
 """compimg exceptions module"""
 import numpy as np
 
+from typing import Sequence
+
 
 class DifferentShapesError(Exception):
-    def __init__(self, shape1, shape2):
-        super().__init__(f"Images have different shapes: {shape1} != {shape2}.")
+    def __init__(self, shape1: Sequence[int], shape2: Sequence[int]):
+        super().__init__(
+            f"Images have different shapes: {shape1} != {shape2}.")
 
 
 class DifferentDTypesError(Exception):
     def __init__(self, dtype1: np.dtype, dtype2: np.dtype):
-        super().__init__(f"Images have different dtypes: {dtype1.name} != {dtype2.name}.")
+        super().__init__(
+            f"Images have different dtypes: {dtype1.name} != {dtype2.name}.")
