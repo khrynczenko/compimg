a7ef32042db8a78685fc52e06a2d363e1f0b59ae
JenioPY <jeniopy@gmail.com>
2019-03-09 17:07:02 +0100

    Squashed commit of the following:
    commit 8f619c34468b86e91ca498f186367f933bf25517
    Author: JenioPY <jeniopy@gmail.com>
    Date:   Sat Mar 9 17:06:15 2019 +0100
    
        More example, docs fix.
    
    commit b04a621882b1c56f69563d2ddb7c93f13c5fff92
    Author: JenioPY <jeniopy@gmail.com>
    Date:   Sat Mar 9 16:28:00 2019 +0100
    
        Added m2r docs building dependency.
    
    commit 87382d83a8c310f399ae0f529bd1abb4a559dbbb
    Author: JenioPY <jeniopy@gmail.com>
    Date:   Sat Mar 9 16:27:11 2019 +0100
    
        Added docs building requirements.txt

--- compimg/__init__.py
+++ compimg/__init__.py
@@ -4,8 +4,21 @@ Here is the simple example of how one can compare one image to another.
 >>> import numpy as np
 >>> from compimg.similarity import MSE
 >>> img = np.ones((10,10), dtype = np.uint8)
->>> error = MSE().compare(img, img)
->>> # comparison of two identical images using MSE so output should be 0
->>> assert error == 0.0
+>>> reference = np.ones((10,10), dtype = np.uint8)
+>>> MSE().compare(img, img)
+0.0
+
+For multiple metrics you could run:
+
+>>> import numpy as np
+>>> from compimg.similarity import MSE, PSNR, SSIM
+>>> for metric in [MSE(), PSNR(), SSIM()]:
+...     img = np.ones((10,10), dtype = np.uint8)
+...     reference = np.zeros((10,10), dtype = np.uint8)
+...     value = metric.compare(img, reference)
+...     print(f"{metric.__class__.__name__} = {value}")
+MSE = 1.0
+PSNR = 48.1308036086791
+SSIM = 0.8667110963012329
 
 """
--- docs/src/conf.py
+++ docs/src/conf.py
@@ -13,7 +13,7 @@
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
 import sys
-sys.path.append("../../")
+sys.path.append(".")
 
 from setuptools import config
 
