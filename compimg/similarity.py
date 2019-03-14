ddb0de92efb74c0a4a77851f9d24acd4fe0f19ea
JenioPY <jeniopy@gmail.com>
2019-03-14 23:34:56 +0100

    Added RMSE, conversion to float32.
    When necessary (overflow/underflow may occur for uint values)
    images are converted to dtype np.float32 when calculating
    metrics.

--- compimg/similarity.py
+++ compimg/similarity.py
@@ -2,7 +2,7 @@
 import abc
 import numpy as np
 
-from numbers import Number
+from numbers import Real
 from compimg._internals import _decorators, _utilities
 from compimg.windows import SlidingWindow, DefaultSlidingWindow
 
@@ -30,15 +30,26 @@ class MSE(SimilarityMetric):
 
     """
 
-    def __init__(self, working_dtype: np.dtype = np.float64):
-        self._working_dtype = working_dtype
+    @_decorators._raise_when_arrays_have_different_dtypes
+    @_decorators._raise_when_arrays_have_different_shapes
+    def compare(self, image: np.ndarray, reference: np.ndarray) -> float:
+        image = image.astype(np.float64, copy=False)
+        reference = reference.astype(np.float64, copy=False)
+        return np.mean(((reference - image) ** 2))
+
+
+class RMSE(SimilarityMetric):
+    """
+    Root mean squared error.
+
+    """
 
     @_decorators._raise_when_arrays_have_different_dtypes
     @_decorators._raise_when_arrays_have_different_shapes
     def compare(self, image: np.ndarray, reference: np.ndarray) -> float:
-        image = image.astype(self._working_dtype, copy=False)
-        reference = reference.astype(self._working_dtype, copy=False)
-        return np.mean(((reference - image) ** 2))
+        image = image.astype(np.float64, copy=False)
+        reference = reference.astype(np.float64, copy=False)
+        return np.sqrt(MSE().compare(image, reference))
 
 
 class MAE(SimilarityMetric):
@@ -47,14 +58,11 @@ class MAE(SimilarityMetric):
 
     """
 
-    def __init__(self, working_dtype: np.dtype = np.float64):
-        self._working_dtype = working_dtype
-
     @_decorators._raise_when_arrays_have_different_dtypes
     @_decorators._raise_when_arrays_have_different_shapes
     def compare(self, image: np.ndarray, reference: np.ndarray) -> float:
-        image = image.astype(self._working_dtype, copy=False)
-        reference = reference.astype(self._working_dtype, copy=False)
+        image = image.astype(np.float64, copy=False)
+        reference = reference.astype(np.float64, copy=False)
         return np.mean(np.abs(reference - image))
 
 
@@ -65,15 +73,12 @@ class PSNR(SimilarityMetric):
 
     """
 
-    def __init__(self, working_dtype: np.dtype = np.float64):
-        self._working_dtype = working_dtype
-
     @_decorators._raise_when_arrays_have_different_dtypes
     @_decorators._raise_when_arrays_have_different_shapes
     def compare(self, image: np.ndarray, reference: np.ndarray) -> float:
         image_original_dtype = image.dtype
-        image = image.astype(self._working_dtype, copy=False)
-        reference = reference.astype(self._working_dtype, copy=False)
+        image = image.astype(np.float64, copy=False)
+        reference = reference.astype(np.float64, copy=False)
         mse = MSE().compare(image, reference)
         if mse == 0.0:
             return float("inf")
@@ -92,19 +97,17 @@ class SSIM(SimilarityMetric):
 
     def __init__(self, k1: float = 0.01, k2: float = 0.03,
                  sliding_window: SlidingWindow = DefaultSlidingWindow(
-                     size=(8, 8), stride=(1, 1)),
-                 working_dtype: np.dtype = np.float64):
+                     size=(8, 8), stride=(1, 1))):
         self._k1 = k1
         self._k2 = k2
         self._sliding_window = sliding_window
-        self._working_dtype = working_dtype
 
     @_decorators._raise_when_arrays_have_different_dtypes
     @_decorators._raise_when_arrays_have_different_shapes
     def compare(self, image: np.ndarray, reference: np.ndarray) -> float:
         _, max_pixel_value = _utilities.get_dtype_range(image.dtype)
-        image = image.astype(self._working_dtype, copy=False)
-        reference = reference.astype(self._working_dtype, copy=False)
+        image = image.astype(np.float64, copy=False)
+        reference = reference.astype(np.float64, copy=False)
         image_windows = self._sliding_window.slide(image)
         reference_windows = self._sliding_window.slide(reference)
         windows_results = []
@@ -116,15 +119,15 @@ class SSIM(SimilarityMetric):
 
     def _calculate_on_window(self, image: np.ndarray,
                              reference: np.ndarray,
-                             max_pixel_value: Number) -> float:
+                             max_pixel_value: Real) -> float:
         x_avg = image.mean()
         y_avg = reference.mean()
         x_var = np.sum((image - x_avg) ** 2) / (image.size - 1)
         y_var = np.sum((reference - y_avg) ** 2) / (reference.size - 1)
         x_y_cov = (np.sum(image - x_avg) * np.sum(reference - y_avg)
                    / (image.size - 1))
-        c1 = (self._k1 * max_pixel_value) ** 2
-        c2 = (self._k2 * max_pixel_value) ** 2
+        c1 = (self._k1 * float(max_pixel_value)) ** 2
+        c2 = (self._k2 * float(max_pixel_value)) ** 2
         nominator = (2.0 * x_avg * y_avg + c1) * (2.0 * x_y_cov + c2)
         denominator = (x_avg ** 2 + y_avg ** 2 + c1) * (
                 x_var ** 2 + y_var ** 2 + c2)
